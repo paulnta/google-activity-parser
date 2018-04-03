be6c77f435795b753b572c4693c25db3deca03ad
paulnta <paul.ntawuruhunga@heig-vd.ch>
2018-04-03 10:19:35 +0200

    make runnable on node>=4

--- bin/google-activity-parser.js
+++ bin/google-activity-parser.js
@@ -1,35 +1,46 @@
 #! /usr/bin/env node
+
 const fs = require('fs')
 const path = require('path')
-const { promisify } = require('util')
-const Parser = require('..')
+const promisify = require('util.promisify')
+const Parser = require('../lib/parser')
 const argv = require('minimist')(process.argv.slice(2), {
-  string: ['input', 'output'],
-  alias: { input: 'i', output: 'o' }
+  string: ['input', 'output', 'help'],
+  boolean: ['help'],
+  alias: { input: 'i', output: 'o', help: 'h' }
 })
 
 const readFile = promisify(fs.readFile)
 const writeFile = promisify(fs.writeFile)
 
-// Check program arguments
-if (!argv['i'] || !argv['o']) {
-  console.log(`You need to provide both arguments: --input file, --output file`)
+if (!argv.input || argv.help) {
+  console.log(
+    'Usage: google-activity-parser [options]\n\n' +
+    'Options:\n' +
+    '-i, --input <file>    input html file to parse\n' +
+    '-o, --output <file>   save as a file. If not provided results will be printed in the console\n' +
+    '-h, --help            show usage'
+  )
   process.exit(1)
 }
 
-const inputFile = path.resolve(argv['i'])
-const outputFile = path.resolve(argv['o'])
+const inputFile = path.resolve(argv.input)
 
 readFile(inputFile)
   .then(data => data.toString())
   .then(html => Parser.parseActivities(html))
-  .then(result => {
-    console.log('result', result)
-    return JSON.stringify(result)
-  })
-  .then(data => writeFile(outputFile, data))
-  .then(() => {
-    console.log('Results written to', outputFile)
+  .then(data => {
+    const filename = argv.output && path.resolve(argv.output)
+    if (filename) {
+      // save as file
+      return writeFile(filename, JSON.stringify(data))
+        .then(() => {
+          console.log(`saved ${data.length} activities in ${filename}`)
+        })
+    }
+    // print in console
+    console.log(data)
+    console.log(`found ${data.length} activities`)
   })
   .catch(err => {
     console.error('Oups an error occured: ', err)
